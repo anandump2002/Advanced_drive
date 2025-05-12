7cf5b77eef04af702b2a89bf842bb819152cd6ee
Anandu M Pillai <anandump2002@gmail.com>
2025-05-12 21:25:00 +0530

    Delete admins directory

--- admins/admin.py
+++
@@ -1,5 +0,0 @@
-# admins/admin.py
-from django.contrib import admin
-from .models import Admin
-
-admin.site.register(Admin)
--- admins/templatetags/form_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='addclass')
-def addclass(field, css_class):
-    if hasattr(field, 'widget'):
-        return field.as_widget(attrs={"class": css_class})
-    return field  # Return unchanged if not a form field
